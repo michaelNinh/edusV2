81e318ef32c0e91a2335fa6075aa4054b0eb07aa
michaelNinh <michaelninh@michaels-MacBook-Pro-2.local>
2017-07-14 19:30:33 -0700

    index is hooked up

--- edus/models.py
+++ edus/models.py
@@ -1,3 +1,62 @@
 from django.db import models
+from datetime import date
+from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
+from django.contrib.auth.models import User  # Blog author or commenter
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=500, null=False)
+    content = models.TextField(max_length=10000, null=False)
+    author = models.ForeignKey('UserEdus', null=False) #what does onDelete model.CASCADE do?
+    solution_found = models.BooleanField(default=False, null=False)
+    points = models.IntegerField(default=0, null=False)
+
+    #how to include media files like pictures??
+
+    #titles could get long...
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.title
+
+class UserEdus(models.Model):
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    bio = models.TextField(max_length=400, help_text="Enter your bio details here.")
+
+    # a feature like this would return the profile page?
+    # def get_absolute_url(self):
+    #     """
+    #     Returns the url to access a particular blog-author instance.
+    #     """
+    #     return reverse('blogs_by_author', args=[str(self.id)])
+
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.user.username
+
+
+class Reply(models.Model):
+    content = models.TextField(max_length=10000, null=False)
+    author = models.ForeignKey(Question, null=False) #what does onDelete model.CASCADE do?
+    correct_answer = models.BooleanField(default=False)
+    points = models.IntegerField(default=0, null=False)
+
+    #how to include media files like pictures??
+
+    #how to represent this in admin dashboard?
+    #could do it by parent Question
+    def __str__(self):
+        """
+        String for representing the Model object.
+        """
+        return self.content
+
+
+
+
+
--- edusProject/settings.py
+++ edusProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'edus.apps.EdusConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'edusProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['./templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
